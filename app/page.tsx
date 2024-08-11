c32f037a06d2870e242092eba5f6ca53e2e68ef5
Sergio Akira Sarui Junior <sergiosaruijr@gmail.com>
2024-08-11 17:55:49 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-import { Button } from './_components/ui/button'
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
   return (
-    <Button>
-      Teste
-    </Button>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Sergio!</h2>
+        <p>Sexta-feira, 09 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner01.png"
+            alt="Agende nos melhores com SOS Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
