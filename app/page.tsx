0c07d293563add92db1dc70e48d86c8e2990dacc
Sergio Akira Sarui Junior <sergiosaruijr@gmail.com>
2024-10-07 16:19:15 -0300

    feat: ensure booking item takes full width on home page

--- app/page.tsx
+++ app/page.tsx
@@ -100,14 +100,19 @@ const Home = async () => {
         </div>
 
         {/* Agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <div className='flex overflow-x-auto gap-3 [&::-webkit-scrollbar]:hidden'>
-           {confirmedBookings.map((booking) => (
-              <BookingItem key={booking.id} booking={booking}  />
-           ))}
-        </div>
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Agendamentos
+            </h2>
+            
+            <div className='flex overflow-x-auto gap-3 [&::-webkit-scrollbar]:hidden'>
+              {confirmedBookings.map((booking) => (
+                  <BookingItem key={booking.id} booking={booking}  />
+              ))}
+            </div>
+          </>
+        )}
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
