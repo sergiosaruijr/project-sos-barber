ea526891856d0321a4b077be99c5a3413302980c
sergiosaruijr <sergiosaruijr@gmail.com>
2025-05-12 20:00:26 -0300

    fix(auth): modified the authentication logic/code

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,22 +1,22 @@
-import { PrismaAdapter } from "@auth/prisma-adapter"
+// import { PrismaAdapter } from "@auth/prisma-adapter"
 import { AuthOptions } from "next-auth"
-import { db } from "./prisma"
-import { Adapter } from "next-auth/adapters"
+// import { db } from "./prisma"
+// import { Adapter } from "next-auth/adapters"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
+          scope: "openid email profile",
           redirect_uri:
-            "https://project-sos-barber.vercel.app/api/auth/callback/google", // URL absoluta!
+            "https://project-sos-barber.vercel.app/api/auth/callback/google", // Fixo!
         },
       },
     }),
@@ -25,13 +25,13 @@ export const authOptions: AuthOptions = {
     sessionToken: {
       name: "__Secure-next-auth.session-token",
       options: {
-        domain: ".vercel.app", // Permite subdomínios
-        path: "/",
+        domain: ".vercel.app", // Permite todos subdomínios
         secure: true,
+        path: "/",
         sameSite: "lax",
       },
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-  debug: true, // Ativa logs detalhados
+  debug: true,
 }
