580200468af05f61bb699d67f06279a0198a5c4d
sergiosaruijr <sergiosaruijr@gmail.com>
2025-05-12 17:42:29 -0300

    fix: correct NEXTAUTH_SECRET environment variable name

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,7 +1,7 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import { AuthOptions } from 'next-auth'
-import { db } from './prisma'
-import { Adapter } from 'next-auth/adapters'
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { AuthOptions } from "next-auth"
+import { db } from "./prisma"
+import { Adapter } from "next-auth/adapters"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
@@ -13,13 +13,13 @@ export const authOptions: AuthOptions = {
     }),
   ],
   callbacks: {
-    async session({session, user}) {
+    async session({ session, user }) {
       session.user = {
         ...session.user,
         id: user.id,
       } as any
       return session
-    }
+    },
   },
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 }
